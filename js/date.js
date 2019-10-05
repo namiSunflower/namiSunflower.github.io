8e8337451f06715825a643af511a0888d63e9ade
Frances Naomi B. Alido <yomi.alido@Francess-MacBook-Pro.local>
2019-10-05 22:59:50 +0800

    up

--- js/date.js
+++ js/date.js
@@ -1,12 +1,11 @@
 var possibilities={
-        day:"numeric",
-        month:"numeric",
-        year:"numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        timeZoneName: "short"
+    day:"numeric",
+    month:"numeric",
+    year:"numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short"
 };
 document.getElementById('output').innerHTML = new Date().toLocaleDateString('en-US', possibilities);
 document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
-
