4148f94797dd0f927df79f8c1975e0b7da23c751
Frances Naomi B. Alido <yomi.alido@Francess-MacBook-Pro.local>
2019-10-22 22:10:14 +0800

    go

--- lesson-6/js/script.js
+++ lesson-6/js/script.js
@@ -19,24 +19,6 @@ window.onload = function() {
     else{
       document.getElementById("pancake").style.display = "none";
     }
-/*Dynamic Weekly Weather*/ 
-        //sunday-monday
-        if (dayOfWeek == 0 || dayOfWeek == 1) {
-        output="Currently: Sunny <br> High: 76℉ <br> Precipitation: 4% <br> Humidity: 69% <br> Wind Speed: 10 mph <br> "}
-        //tuesday
-        else if (dayOfWeek == 2) {
-        output="Currently: Rainy <br> High: 71°F <br>Precipitation: 4% <br> Humidity: 59% <br> Wind Speed: 9 mph"}
-        //wednesday
-        else if (dayOfWeek == 3) {
-        output="Currently: Windy <br> High: 70°F <br> Precipitation: 8% <br> Humidity: 60% <br> Wind Speed: 15 mph"}
-        //thursday
-        else if (dayOfWeek == 4) {
-        output="Currently: Stormy <br> High: 68°F <br> Precipitation: 6%<br> Humidity: 51% <br> Wind Speed: 13 mph"}
-        //friday-sat
-        else{output="Currently: Sunny <br> High: 71°F <br> Precipitation: 5% <br> Humidity: 55% <br> Wind Speed: 10 mph"};
-        
-        //Output
-        document.getElementById('dynamic-weather').innerHTML=output;
     }
   
 
