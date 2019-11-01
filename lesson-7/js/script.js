a0ebf9e7416aacf172b9ea8a8ae23594db11cb4a
Frances Naomi B. Alido <yomi.alido@Francess-MacBook-Pro.local>
2019-11-01 21:01:01 +0800

    updates

--- lesson-7/js/script.js
+++ lesson-7/js/script.js
@@ -1,57 +1,25 @@
+
 function toggleMenu(){
     document.getElementById("hamburger").classList.toggle("change");
     document.getElementById("nav-links").classList.toggle("change");
 }
-var possibilities={
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
+/*LAZY LOADING */
+window.onscroll=function(ev){
+  lazyload();
 };
-document.getElementById('output').innerHTML = new Date().toLocaleDateString('en-US', possibilities);
-document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
-  
+function lazyload(){
+   const lazyImage = document.getElementsByClassName('lazy');
+   for (var i=0;i<lazyImage.length;i++){
+       if(elementInViewport(lazyImage[i])){
+           lazyImage[i].setAttribute('src', lazyImage[i].getAttribute('data-src'));
+}}}
 
-//saturday pancake banner
-var dayOfWeek = new Date().getDay();
-   window.onload = function pancake() {
-    if (dayOfWeek == 5){
-        document.getElementById("pancake").style.display = "block";
-    }
-    else{
-      document.getElementById("pancake").style.display = "none";
-    }
+function elementInViewport(el){
+   const rect = el.getBoundingClientRect();
+   return(
+       rect.top >=0 &&
+       rect.left >=0 &&
+       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+   );
 }
-function addLoadEvent(func) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-      window.onload = func;
-    } else {
-      window.onload = function() {
-        if (oldonload) {
-          oldonload();
-        }
-        func();
-      }
-    }
-  }
-  
- /*Dynamic Weather*/ 
-function weather(){
-    if(dayOfWeek == 2){
-        document.getElementById('weather').innerHTML="Rainy";
-    }
-    else if(dayOfWeek == 3){
-        document.getElementById('weather').innerHTML="Windy";
-    }
-    else if(dayOfWeek == 4){
-        document.getElementById('weather').innerHTML="Stormy";
-    }
-    else{
-        document.getElementById('weather').innerHTML="Sunny";
-    }
-}
-
-addLoadEvent(weather);
-addLoadEvent(function(){
-});
