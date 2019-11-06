9ac482d7b17dedc0f8b8bcc15c55cb2203e49a51
Frances Naomi B. Alido <yomi.alido@Francess-MacBook-Pro.local>
2019-11-06 16:30:37 +0800

    update

---
+++ lesson-8/js/script.js
@@ -0,0 +1,25 @@
+//nav-bar
+function toggleMenu(){
+    document.getElementById("hamburger").classList.toggle("change");
+    document.getElementById("nav-links").classList.toggle("change");
+}
+//footer js
+var possibilities={
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+document.getElementById('output').innerHTML = new Date().toLocaleDateString('en-US', possibilities);
+document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
+
+//saturday pancake banner
+var dayOfWeek = new Date().getDay();
+   window.onload = function pancake() {
+    if (dayOfWeek == 5){
+        document.getElementById("pancake").style.display = "block";
+    }
+    else{
+      document.getElementById("pancake").style.display = "none";
+    }
+}
