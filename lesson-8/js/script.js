a0e2d881228cfd9715901bd14429a4ca6c251054
Frances Naomi B. Alido <yomi.alido@Francess-MacBook-Pro.local>
2019-11-13 02:01:28 +0800

    push

--- lesson-8/js/script.js
+++ lesson-8/js/script.js
@@ -27,3 +27,15 @@ var dayOfWeek = new Date().getDay();
 function adjustRating(rating) {
   document.getElementById("ratingvalue").innerHTML = rating;
 }
+function validate_form() {
+  let full_name, phone_number, zip;
+  full_name = document.getElementById('fullname');
+  phone_number = document.getElementById('phone_number');
+  zip = document.getElementById('zip');
+
+
+  if (full_name.checkValidity() && phone_number.checkValidity() && zip.checkValidity()) {
+      let win = window.open('thanks.html', '_blank');
+      win.focus();
+  }
+}
