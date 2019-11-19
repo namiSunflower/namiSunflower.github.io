9d7312063426346a3877744020e2ce7f8b7bcfbc
Frances Naomi B. Alido <yomi.alido@Francess-MacBook-Pro.local>
2019-11-19 22:07:48 +0800

    update

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -8,7 +8,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?&id=5604473&units=imperia
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
         const desc = jsObject.weather[0].description;  // note how we reference the weather array
         document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-        document.getElementById('weather icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-        document.getElementById('weather icon').setAttribute('alt', desc);
+        document.getElementById('weather_icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+        document.getElementById('weather_icon').setAttribute('alt', desc);
     });
 
